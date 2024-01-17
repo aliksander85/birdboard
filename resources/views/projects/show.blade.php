ce5a3f9e5e4ee51205cd3ae5ffdfbfb352a31c4c
Alexander Potapov <potapov.av.85@gmail.com>
2024-01-17 15:02:39 +0400

    [refactor]: added error showing on edit project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -64,6 +64,14 @@
 
                     <button type="submit" class="button">Save</button>
                 </form>
+
+                @if ($errors->any())
+                    <div class="field mt-6">
+                        @foreach ($errors->all() as $error)
+                            <li class="text-sm text-red-50">{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
             </section>
         </div>
 
